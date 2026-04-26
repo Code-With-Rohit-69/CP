52d784bc47e05c61992ba892f538bc56e6a200c4
Rohit Singh <rr2488017@gmail.com>
2026-04-26 10:17:48 +0530

    Refactor B_Sort_the_Array to simplify logic and improve readability

--- B_Sort_the_Array.java
+++ B_Sort_the_Array.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class B_Sort_the_Array {
 
+    public static void reverse(int[] arr, int l, int r) {
+        int i = l, j = r;
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -13,29 +27,36 @@ public class B_Sort_the_Array {
             arr[i] = sc.nextInt();
         }
 
-        int count = 0;
-        int c = 0;
+        int l = 0;
 
-        for (int i = 1; i < n; i++) {
-            if(arr[i] < arr[i - 1]) {
-                c++;
-            } else {
-                if(c > 0) {
-                    c = 0;
-                    count++;
-                }
+        while (l < n - 1 && arr[l] < arr[l + 1]) {
+            l++;
+        }
+
+        if(l == n - 1) {
+            System.out.println("yes");
+            System.out.println("1 1");
+            return;
+        }
+
+        int r = l;
+
+        while (r < n - 1 && arr[r] > arr[r + 1]) {
+            r++;
+        }
+
+        reverse(arr, l, r);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                System.out.println("no");
+                sc.close();
+                return;
             }
         }
 
-        if(c > 0) {
-            count++;
-        }
-
-        if (count < 2) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
+        System.out.println("yes");
+        System.out.println((l + 1) + " " + (r + 1));
 
         sc.close();
     }
