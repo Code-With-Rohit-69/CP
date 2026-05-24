a55a0c361343d89c6e739f9ea2085826ef60def8
Rohit Singh <rr2488017@gmail.com>
2026-05-24 07:52:20 +0530

    Refactor B_Taxi solution to improve logic for counting cars and handling input

--- B_Taxi.java
+++ B_Taxi.java
@@ -9,25 +9,65 @@ public class B_Taxi {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        int cars = 0;
-        int remaining = 0;
 
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+        int four = 0;
+        int three = 0;
+        int two = 0;
+        int one = 0;
 
         for (int i = 0; i < n; i++) {
-            if(arr[i] > remaining) {
-                cars++;
-                remaining = 4;
-            }
+            arr[i] = sc.nextInt();
 
-            remaining -= arr[i];
+            if (arr[i] == 3) {
+                three++;
+            } else if (arr[i] == 4) {
+                four++;
+            } else if (arr[i] == 2) {
+                two++;
+            } else {
+                one++;
+            }
         }
-        
+
+        int cars = four;
+
+        // 3 with 1
+
+        int rem = 0;
+
+        if (three == one) {
+            cars += three;
+        } else if (three > one) {
+            cars += one;
+            three -= one;
+            cars += three;
+        } else {
+            cars += three;
+            rem = one - three;
+        }
+
+        // only 2 and remaining 1s
+
+        int multiple2s = two * 2;
+
+        cars += multiple2s / 4;
+
+        rem += multiple2s % 4;
+
+        // remaining
+
+        if (rem > 0) {
+            if (rem % 4 == 0) {
+                cars += rem / 4;
+            } else {
+                cars += rem / 4 + 1;
+            }
+        }
+
         System.out.println(cars);
 
         sc.close();
     }
 }
+
+// 1 1 2 2 3 3 4 4
