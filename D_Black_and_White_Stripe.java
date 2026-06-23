2f15dda4bd48a5c79f1dd568975b3ef222c20b11
Rohit Singh <rr2488017@gmail.com>
2026-06-23 15:29:20 +0530

    Add A_Add_and_Divide and D_Black_and_White_Stripe classes with implementations for respective problem solutions

---
+++ D_Black_and_White_Stripe.java
@@ -0,0 +1,137 @@
+import java.io.*;
+
+public class D_Black_and_White_Stripe {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = fr.nextInt();
+
+        while (t-- > 0) {
+            int n = fr.nextInt();
+            int k = fr.nextInt();
+
+            String s = fr.next();
+
+            int left = 0;
+            int w = 0;
+
+            int count = Integer.MAX_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+
+                if(ch == 'W') w++;
+
+                while (i - left + 1 > k && left <= i) {
+                    if (s.charAt(left) == 'W') {
+                        w--;
+                    }
+
+                    left++;
+                }
+
+                if (i - left + 1 == k) {
+                    count = Math.min(count, w);
+                }
+
+            }
+
+            out.println(count);
+
+        }
+
+        out.flush();
+    }
+}
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
