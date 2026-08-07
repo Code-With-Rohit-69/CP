2bfbdd124e8a3931ba65b634651cd3d26c7b7372
Rohit Singh <rr2488017@gmail.com>
2026-08-07 11:48:25 +0530

    hell

--- D_Deletive_Editing.java
+++ D_Deletive_Editing.java
@@ -14,25 +14,25 @@ public class D_Deletive_Editing {
             int n = s.length() - 1;
             int m = t.length() - 1;
 
-            int[] f = new int[26];
+            int[] freq = new int[26];
 
             while (n >= 0 && m >= 0) {
                 if(s.charAt(n) == t.charAt(m)) {
-                    if (f[s.charAt(n) - 'A'] > 0) {
+                    if (freq[s.charAt(n) - 'A'] > 0) {
                         break;
                     }
 
-                    f[s.charAt(n) - 'A'] = Math.max(0, f[s.charAt(n) - 'A']--);
+                    freq[s.charAt(n) - 'A'] = Math.max(0, freq[s.charAt(n) - 'A']--);
 
                     n--;
                     m--;
                 } else {
-                    f[s.charAt(n) - 'A'] += 1;
+                    freq[s.charAt(n) - 'A'] += 1;
                     n--;
                 }
             }
 
-            // for (int i = 0; i < f.length; i++) {
+            // for (int i = 0; i < freq.length; i++) {
             //     System.out.print(((char) (i + 'A')) + " -> " + f[i] + ", ");
             // }
 
