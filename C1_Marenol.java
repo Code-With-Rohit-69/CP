28fae736a9d0b19ee66c97cc3133cb324c8985e8
Rohit Singh <rr2488017@gmail.com>
2026-08-12 08:56:44 +0530

    Add C1_Marenol class with main method and FastReader utility for input handling

---
+++ C1_Marenol.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.util.*;
+
+public class C1_Marenol {
+    
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+        StringBuilder out = new StringBuilder();
+
+        int t = fr.nextInt();
+
+        while (t-- > 0) {
+            int n = fr.nextInt();
+
+            String A = fr.next();
+            String B = fr.next();
+
+            int onesA = 0, onesB = 0;
+            int evenA1s = 0, evenB1s = 0;
+
+            for(int i = 0; i < n; i++) {
+                onesA += (A.charAt(i) - '0');
+                onesB += (B.charAt(i) - '0');
+
+                evenA1s = A.charAt(i) == '1' && (i & 1) == 0 ? evenA1s + 1 : evenA1s;
+                evenB1s = B.charAt(i) == '1' && (i & 1) == 0 ? evenB1s + 1 : evenB1s;
+
+            }
+
+            int oddA1s = onesA - evenA1s, oddB1s = onesB - evenB1s;
+
+            if (onesA != onesB) {
+                out.append("NO \n");
+                continue;
+            }
+
+            if (evenA1s != evenB1s || oddA1s != oddB1s) {
+                out.append("NO \n");
+            } else {
+                out.append("YES \n");
+            }
+
+        }
+
+        System.out.println(out);
+
+    }
+}
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
+
+
+
