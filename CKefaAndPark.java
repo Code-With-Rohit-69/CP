fdddfd960210f538f5048d963528578f7fa526cc
Rohit Singh <rr2488017@gmail.com>
2026-08-29 09:55:02 +0530

    Add BModuloSum and CKefaAndPark classes with input handling and processing logic

---
+++ CKefaAndPark.java
@@ -0,0 +1,169 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * CKefaAndPark
+ */
+public class CKefaAndPark {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+
+        int n = fr.nextInt(), m = fr.nextInt();
+        int[] cat = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            cat[i] = fr.nextInt();
+        }
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = fr.nextInt() - 1;
+            int v = fr.nextInt() - 1;
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[] vis = new boolean[n];
+
+        if (cat[0] <= m) {
+            q.add(new int[] { 0, cat[0] });
+            vis[0] = true;
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.remove();
+
+            int node = curr[0];
+            int cats = curr[1];
+
+            boolean leaf = true;
+
+            for (int neigh : graph.get(node)) {
+                if (!vis[neigh]) {
+                    leaf = false;
+
+                    int newCats = cat[neigh] == 0 ? 0 : cats + 1;
+
+                    if (newCats > m) {
+                        continue;
+                    }
+
+                    q.add(new int[] { neigh, newCats });
+                    vis[neigh] = true;
+
+                }
+
+            }
+
+            if(leaf && node != 0) count++;
+
+        }
+
+        System.out.println(count);
+
+    }
+}
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1)
+                return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1)
+                return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1)
+                return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1)
+                return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
