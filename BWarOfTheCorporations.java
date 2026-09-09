660488e1d5b3c7ec55d8e0304867415ba9624378
Rohit Singh <rr2488017@gmail.com>
2026-09-09 09:52:52 +0530

    Add BWarOfTheCorporations and CExams solutions

---
+++ BWarOfTheCorporations.java
@@ -0,0 +1,120 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * BWarOfTheCorporations
+ */
+public class BWarOfTheCorporations {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+
+        String s = fr.next();
+        String t = fr.next();
+
+        int count = 0;
+
+        for (int i = 0; i < s.length() - t.length() + 1; ) {
+            String sub = s.substring(i, i + t.length());
+            if(sub.equals(t)) {
+                count++;
+                i += t.length();
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
---
+++ CExams.java
@@ -0,0 +1,136 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * CExams
+ */
+public class CExams {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+
+        int n = fr.nextInt();
+
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            int a = fr.nextInt();
+            int b = fr.nextInt();
+
+            arr[i][0] = a;
+            arr[i][1] = b;
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int day = -1;
+
+        for(int[] A : arr) {
+            int a = A[0], b = A[1];
+
+            if(b >= day) {
+                day = b;
+            } else {
+                day = a;
+            }
+
+        }
+
+        System.out.println(day);
+
+    }
+}
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
