c570f9d27f50c10e7570b4c157b207b39791cbff
Rohit Singh <rr2488017@gmail.com>
2026-09-12 09:55:16 +0530

    Add APashmakAndGarden solution with FastReader utility

---
+++ APashmakAndGarden.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * APashmakAndGarden
+ */
+
+/*
+    the difference b\w two coordinates will always be same
+
+*/
+
+public class APashmakAndGarden {
+
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+
+        int x1 = fr.nextInt();
+        int y1 = fr.nextInt();
+        int x2 = fr.nextInt();
+        int y2 = fr.nextInt();
+
+        int xDiff = Math.abs(x2 - x1);
+        int yDiff = Math.abs(y2 - y1);
+
+        
+
+
+
+    }
+}
+
+/*
+
+    x1 = 2, y1 = 3
+    x2 = 4, y2 = 2
+
+    -----------------------
+
+    x3 = 2, y3 = 2
+    x4 = 4, y4 = 3
+
+    -----------------------
+
+    xDiff = 2
+    yDiff = 1
+
+
+
+*/
+
+class FastReader {
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int ptr = 0, len = 0;
+
+    private int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0)
+                return -1;
+        }
+        return buffer[ptr++];
+    }
+
+    int nextInt() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        int num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    long nextLong() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        long num = 0;
+        while (c > ' ') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        return num * sign;
+    }
+
+    String next() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    double nextDouble() throws IOException {
+        int c;
+        while ((c = read()) <= ' ') {
+            if (c == -1) return -1;
+        }
+        int sign = 1;
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        double num = 0;
+        while (c > ' ' && c != '.') {
+            num = num * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            double div = 10;
+            while ((c = read()) > ' ') {
+                num += (c - '0') / div;
+                div *= 10;
+            }
+        }
+        return num * sign;
+    }
+}
